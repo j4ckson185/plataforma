e6561b6002f2499428792711f3b363f169de49f7
j4ckson185 <jackson_division@hotmail.com>
2024-07-02 02:04:48 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,29 @@ from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, messaging
 from websocket import socketio
+from dotenv import load_dotenv
+import os
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
 
 # Configurações do Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-here'
 
 # Inicialização do Firebase Admin SDK
-cred = credentials.Certificate("C:\\xampp\\plataforma motoboys\\Delivery\\motoboys-a41e6-firebase-adminsdk-dr0g8-084b1f32d6.json")
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+})
 firebase_admin.initialize_app(cred)
 
 # Rota para criar pedidos
